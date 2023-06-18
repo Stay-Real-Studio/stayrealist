d6ede45193903e1d1dfc538d3f3938befbdc283b
Hao Hao <haohao@stayreal.studio>
2023-06-17 17:01:21 -0700

    map starts to work

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,14 +1,20 @@
-"use client";
+'use client';
 
-import { Button } from "ui";
+import { Button } from 'ui';
 
-import styles from "../styles/index.module.css";
+import styles from '../styles/index.module.css';
+import dynamic from 'next/dynamic';
+
+const SrlMap = dynamic(() => import('../components/map.component'), {
+  ssr: false,
+});
 
 export default function Web() {
   return (
     <div className={styles.container}>
       <h1>Web</h1>
-      <Button onClick={() => console.log("Pressed!")} text="Boop" />
+      <Button onClick={() => console.log('Pressed!')} text="Boop" />
+      <SrlMap />
     </div>
   );
 }
