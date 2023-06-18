adb679578195cc3652c65b288b60f68ca5b72774
Hao Hao <haohao@stayreal.studio>
2023-06-17 19:04:48 -0700

    cleanup nativewind config

--- apps/native/App.tsx
+++ apps/native/App.tsx
@@ -1,18 +1,16 @@
-import { StyleSheet, View, Text, Dimensions } from 'react-native';
+import { View, Text } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import MapView from 'react-native-maps';
-import { styled } from 'nativewind';
-
-const StyledView = styled(View)
-const StyledText = styled(Text)
 
 export default function Native() {
   return (
-    <View style={styles.container}>
-      <StyledView className="flex-1 items-center justify-center">
-        <StyledText className="StyledText-slate-800">isLoading</StyledText>
-      </StyledView>
-      <MapView style={styles.map}
+    <View className='flex items-center justify-center'>
+      <View className="flex-1">
+        <Text className="Text-slate-800">isLoading</Text>
+      </View>
+      
+      <MapView
+        className="h-full w-full"
         initialRegion={{
           latitude: 37.78825,
           longitude: -122.4324,
@@ -20,26 +18,8 @@ export default function Native() {
           longitudeDelta: 0.0421,
         }}
       />
-      
+
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  header: {
-    fontWeight: 'bold',
-    marginBottom: 20,
-    fontSize: 36,
-  },
-  map: {
-    height: Dimensions.get('window').height,
-    width: Dimensions.get('window').width,
-  },
-});
---
+++ apps/native/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [],
+  content: [
+    './App.{js,jsx,ts,tsx}',
+    './<custom directory>/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
