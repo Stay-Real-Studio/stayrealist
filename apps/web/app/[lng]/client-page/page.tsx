c2e238d6d596b427ae6f39ae662155d9396fdf07
tianxiaxi <warren@stayreal.studio>
2023-06-27 22:44:38 +0800

    first version of logo

--- apps/web/app/[lng]/client-page/page.tsx
+++ apps/web/app/[lng]/client-page/page.tsx
@@ -1,24 +1,26 @@
-'use client';
+"use client";
 
-import Link from 'next/link';
-import { useTranslation } from '../../i18n/client';
-import { Footer } from '../components/Footer/client';
-import { LanguageType } from '../../../types/i18n.types';
-import SrMap from '../../../components/map';
+import Link from "next/link";
+import { useTranslation } from "../../i18n/client";
+import { Footer } from "../components/Footer/client";
+import { LanguageType } from "../../../types/i18n.types";
+import SrMap from "../../../components/map";
+import { Logo } from "../components/Logo/client";
 
 export default function Page({
   params: { lng },
 }: {
   params: { lng: LanguageType };
 }) {
-  const { t } = useTranslation(lng, 'client-page');
+  const { t } = useTranslation(lng, "client-page");
   return (
     <>
+      <Logo lng={lng} />
       <div className="z-10">
-        <h1 className="text-3xl font-bold">{t('title')}</h1>
+        <h1 className="text-3xl font-bold">{t("title")}</h1>
         <Link href={`/${lng}`}>
           <button type="button" className="text-3xl font-bold underline">
-            {t('back-to-home')}
+            {t("back-to-home")}
           </button>
         </Link>
       </div>
--- apps/web/app/[lng]/page.tsx
+++ apps/web/app/[lng]/page.tsx
@@ -1,7 +1,8 @@
-import Link from 'next/link';
-import { useTranslation } from '../i18n';
-import { getCoins } from '../../composables/coin.functions';
-import { Footer } from './components/Footer';
+import Link from "next/link";
+import { useTranslation } from "../i18n";
+import { getCoins } from "../../composables/coin.functions";
+import { Footer } from "./components/Footer";
+import { Logo } from "./components/Logo";
 
 export default async function Page({ params: { lng } }) {
   const { t } = await useTranslation(lng);
@@ -9,13 +10,14 @@ export default async function Page({ params: { lng } }) {
 
   return (
     <>
-      <h1>{t('title')}</h1>
+      <Logo lng={lng} />
+      <h1>{t("title")}</h1>
       {coins.result.map((coin) => {
         return <div key={coin._rev}>{coin.name}</div>;
       })}
-      <Link href={`/${lng}/second-page`}>{t('to-second-page')}</Link>
+      <Link href={`/${lng}/second-page`}>{t("to-second-page")}</Link>
       <br />
-      <Link href={`/${lng}/client-page`}>{t('to-map-page')}</Link>
+      <Link href={`/${lng}/client-page`}>{t("to-map-page")}</Link>
       <Footer lng={lng} />
     </>
   );
