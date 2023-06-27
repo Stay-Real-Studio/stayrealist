4d0ade3e78b3c6026afd4413f1b3ad81db89f17d
Hao Hao <haohao@stayreal.studio>
2023-06-27 08:48:19 -0700

    code cleanup

--- apps/web/app/[lng]/client-page/page.tsx
+++ apps/web/app/[lng]/client-page/page.tsx
@@ -7,27 +7,24 @@ import { LanguageType } from '../../../types/i18n.types'
 import SrMap from '../../../components/map'
 
 export default function Page({
-    params: { lng },
+  params: { lng },
 }: {
-    params: { lng: LanguageType }
+  params: { lng: LanguageType }
 }) {
-    const { t } = useTranslation(lng, 'client-page')
-    return (
-        <>
-            <div className="z-10">
-                <h1 className="text-3xl font-bold">{t('title')}</h1>
-                <Link href={`/${lng}`}>
-                    <button
-                        type="button"
-                        className="text-3xl font-bold underline"
-                    >
-                        {t('back-to-home')}
-                    </button>
-                </Link>
-            </div>
-            <SrMap />
+  const { t } = useTranslation(lng, 'client-page')
+  return (
+    <>
+      <div className="z-10">
+        <h1 className="text-3xl font-bold">{t('title')}</h1>
+        <Link href={`/${lng}`}>
+          <button type="button" className="text-3xl font-bold underline">
+            {t('back-to-home')}
+          </button>
+        </Link>
+      </div>
+      <SrMap />
 
-            <Footer lng={lng} />
-        </>
-    )
+      <Footer lng={lng} />
+    </>
+  )
 }
