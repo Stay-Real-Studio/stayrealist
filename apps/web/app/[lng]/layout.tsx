05b74ef384253b86d519295bffad0d22808cf310
Hao Hao <haohao@stayreal.studio>
2023-06-27 08:44:42 -0700

    move logo into layout

--- apps/web/app/[lng]/layout.tsx
+++ apps/web/app/[lng]/layout.tsx
@@ -1,18 +1,22 @@
-import { dir } from 'i18next';
-import Providers from '../../components/providers';
+import { dir } from 'i18next'
+import Providers from '../../components/providers'
+import { LanguageType } from '../../types/i18n.types'
+import { CnLogo } from './components/CnLogo'
+import { EnLogo } from './components/EnLogo'
 
-const languages = ['en', 'zh-CN'];
+const languages = ['en', 'zh-CN']
 
 export async function generateStaticParams() {
-  return languages.map((lng) => ({ lng }));
+  return languages.map((lng) => ({ lng }))
 }
 
 export default function RootLayout({ children, params: { lng } }) {
   return (
     <html lang={lng} dir={dir(lng)}>
       <body>
+        {lng === LanguageType.English ? <EnLogo /> : <CnLogo />}
         <Providers>{children}</Providers>
       </body>
     </html>
-  );
+  )
 }
--- apps/web/app/[lng]/page.tsx
+++ apps/web/app/[lng]/page.tsx
@@ -2,25 +2,21 @@ import Link from 'next/link'
 import { useTranslation } from '../i18n'
 import { getCoins } from '../../composables/coin.functions'
 import { Footer } from './components/Footer'
-import { LanguageType } from '../../types/i18n.types'
-import { EnLogo } from './components/EnLogo'
-import { CnLogo } from './components/CnLogo'
 
 export default async function Page({ params: { lng } }) {
-    const { t } = await useTranslation(lng)
-    const coins = await getCoins()
+  const { t } = await useTranslation(lng)
+  const coins = await getCoins()
 
-    return (
-        <>
-            {lng === LanguageType.English ? <EnLogo /> : <CnLogo />}
-            <h1>{t('title')}</h1>
-            {coins.result.map((coin) => {
-                return <div key={coin._rev}>{coin.name}</div>
-            })}
-            <Link href={`/${lng}/second-page`}>{t('to-second-page')}</Link>
-            <br />
-            <Link href={`/${lng}/client-page`}>{t('to-map-page')}</Link>
-            <Footer lng={lng} />
-        </>
-    )
+  return (
+    <>
+      <h1>{t('title')}</h1>
+      {coins.result.map((coin) => {
+        return <div key={coin._rev}>{coin.name}</div>
+      })}
+      <Link href={`/${lng}/second-page`}>{t('to-second-page')}</Link>
+      <br />
+      <Link href={`/${lng}/client-page`}>{t('to-map-page')}</Link>
+      <Footer lng={lng} />
+    </>
+  )
 }
