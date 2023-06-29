01ba56e661bb2db01ca7dc89bf0d20ed236939ca
Hao Hao <haohao@stayreal.studio>
2023-06-29 15:18:23 -0700

    removed shopDetails

--- apps/web/components/ShopDetailsContainer.tsx
+++ apps/web/components/ShopDetailsContainer.tsx
@@ -7,7 +7,7 @@ export const ShopDetailsContainer = ({ shop }: { shop: Shop }) => {
   return (
     <>
       <WhatsappShareButton
-        url={'https://github.com/next-share'}
+        url={'http://localhost:3000/en/shops/3e30d82a-2111-4e51-bc2e-192569caafb6'}
         title={'next-share is a social share buttons for your next React apps.'}
         separator=":: "
       >
