2f6a38f38e0fe554f80d6eefde4adde3297be4a4
Hao Hao <haohao@stayreal.studio>
2023-06-29 12:06:09 -0700

    add react native ui kitten into native

--- apps/native/App.tsx
+++ apps/native/App.tsx
@@ -1,24 +1,24 @@
-import { View, Text } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
-import MapView from 'react-native-maps';
+import { View, Text } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
+import { ApplicationProvider, Layout } from '@ui-kitten/components'
+import * as eva from '@eva-design/eva'
 
 export default function Native() {
   return (
-    <View>
-      <View >
-        <Text>isLoading</Text>
-      </View>
-      
-      <MapView
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      />
-
+    <>
+      <ApplicationProvider {...eva} theme={eva.light}>
+        <Layout
+          style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
+        >
+          <View>
+            <View>
+              <Text>isLoading</Text>
+            </View>
+            <Text>HOME</Text>
+          </View>
+        </Layout>
+      </ApplicationProvider>
       <StatusBar />
-    </View>
-  );
+    </>
+  )
 }
