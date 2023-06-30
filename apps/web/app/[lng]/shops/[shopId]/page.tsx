6b8c3bd797c43233b8522eeedde0df2231e32ae0
Hao Hao <haohao@stayreal.studio>
2023-06-30 14:22:39 -0700

    update whatsapp share w and h

--- apps/web/app/[lng]/shops/[shopId]/page.tsx
+++ apps/web/app/[lng]/shops/[shopId]/page.tsx
@@ -22,8 +22,8 @@ export async function generateMetadata(
       images: [
         {
           url: 'https://images.unsplash.com/photo-1540487482501-751a27ab6a7d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80',
-          width: 800,
-          height: 600,
+          width: 256,
+          height: 256,
         },
       ],
       locale: lng,
