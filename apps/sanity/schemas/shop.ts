cf17f16de9dc495c84e8c9412dd4710ce76f0cf6
Hao Hao <haohao@stayreal.studio>
2023-06-30 23:46:16 -0700

    model shops

--- apps/sanity/schemas/shop.ts
+++ apps/sanity/schemas/shop.ts
@@ -17,7 +17,40 @@ export default {
     {
       title: 'Shop Location',
       name: 'location',
-      type: 'geopoint'
-    }
+      type: 'geopoint',
+    },
+    {
+      title: 'Shop Logo',
+      name: 'logo',
+      type: 'image',
+    },
+    {
+      title: 'Shop Background Image',
+      name: 'backgroundImage',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      title: 'Accept Coins',
+      name: 'acceptCoins',
+      type: 'array',
+      of: [
+        {
+          type: 'reference',
+          to: [{type: 'coin'}],
+        },
+      ],
+    },
+    {
+      title: 'Tags',
+      name: 'tags',
+      type: 'array',
+      of: [{type: 'string'}],
+      options: {
+        layout: 'tags',
+      },
+    },
   ],
 }
