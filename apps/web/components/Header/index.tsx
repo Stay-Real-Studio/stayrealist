47a898b450a7ee21e95d20293e943dcef155e439
tianxiaxi <warren@stayreal.studio>
2023-07-03 23:27:04 +0800

    web ui optimization

--- apps/web/components/Header/index.tsx
+++ apps/web/components/Header/index.tsx
@@ -3,6 +3,9 @@ import { LanguageType } from '../../types/i18n.types'
 import { CnLogo } from '../logos/CnLogo'
 import { EnLogo } from '../logos/EnLogo'
 import LanguageList from '../template/LanList'
+import LogoJPG from '../../assets/images/logo.jpg'
+import Image from 'next/image'
+import Link from 'next/link'
 
 export const Header = async ({ lng }) => {
   const { t } = await useTranslation(lng)
@@ -14,21 +17,32 @@ export const Header = async ({ lng }) => {
   ]
   return (
     <>
-      <header className="flex w-full absolute top-8 justify-between items-center z-10">
-        <span className="logo w-40">
-          {lng === LanguageType.English ? (
-            <EnLogo lng={lng} />
-          ) : (
-            <CnLogo lng={lng} />
-          )}
-        </span>
+      <header className="flex w-full fixed justify-between py-8 top-0 items-center z-10 px-8 bg-white">
+        <div className="w-16 sm:w-48 flex items-center">
+          <Link href={`/${lng}/`}>
+            <Image
+              className="rounded-full w-8 h-8 cursor-pointer"
+              src={LogoJPG}
+              width={100}
+              height={100}
+              alt={'logo'}
+            />
+          </Link>
+          <span className="logo hidden sm:inline-block text-sm sm:text-base">
+            {lng === LanguageType.English ? (
+              <EnLogo lng={lng} />
+            ) : (
+              <CnLogo lng={lng} />
+            )}
+          </span>
+        </div>
 
-        <div className="hidden sm:flex sm:gap-x-2 md:gap-x-5 lg:gap-x-10 flex-1 justify-center items-center">
+        <div className="sm:text-base text-sm hidden sm:flex sm:gap-x-2 md:gap-x-5 lg:gap-x-10 flex-1 justify-center items-center">
           {navigation.map((item) => (
             <a
               key={item.name}
               href={item.href}
-              className="text-sm font-semibold leading-6 text-gray-900 "
+              className="font-semibold leading-6 text-gray-900 "
             >
               {item.name}
             </a>
--- apps/web/components/share-buttons.client.tsx
+++ apps/web/components/share-buttons.client.tsx
@@ -21,28 +21,40 @@ export const ShareButtons = ({ lng, shop }: { lng: any; shop: Shop }) => {
   const title = `${shop.name} is recommanded by Stayrealist`
   return (
     <>
-      <WhatsappShareButton url={url} title={title} separator=":: ">
-        <WhatsappIcon size={32} round />
-      </WhatsappShareButton>
-      <TwitterShareButton url={url} title={title}>
-        <TwitterIcon size={32} round />
-      </TwitterShareButton>
-      <EmailShareButton url={url} subject={title} body="body">
-        <EmailIcon size={32} round />
-      </EmailShareButton>
-      <RedditShareButton url={url} title={title}>
-        <RedditIcon size={32} round />
-      </RedditShareButton>
-      <TelegramShareButton url={url} title={title}>
-        <TelegramIcon size={32} round />
-      </TelegramShareButton>
-      <WeiboShareButton
-        url={url}
-        title={title}
-        // image={`${String(window.location)}/${example - image}`}
-      >
-        <WeiboIcon size={32} round />
-      </WeiboShareButton>
+      <div className="mr-4">
+        <WhatsappShareButton url={url} title={title} separator=":: ">
+          <WhatsappIcon size={32} round />
+        </WhatsappShareButton>
+      </div>
+      <div className="mr-4">
+        <TwitterShareButton url={url} title={title}>
+          <TwitterIcon size={32} round />
+        </TwitterShareButton>
+      </div>
+      <div className="mr-4">
+        <EmailShareButton url={url} subject={title} body="body">
+          <EmailIcon size={32} round />
+        </EmailShareButton>
+      </div>
+      <div className="mr-4">
+        <RedditShareButton url={url} title={title}>
+          <RedditIcon size={32} round />
+        </RedditShareButton>
+      </div>
+      <div className="mr-4">
+        <TelegramShareButton url={url} title={title}>
+          <TelegramIcon size={32} round />
+        </TelegramShareButton>
+      </div>
+      <div className="mr-4">
+        <WeiboShareButton
+          url={url}
+          title={title}
+          // image={`${String(window.location)}/${example - image}`}
+        >
+          <WeiboIcon size={32} round />
+        </WeiboShareButton>
+      </div>
     </>
   )
 }
